b8bee0130947888d270eedd55221dc9f3bf3391d
BugHunter-X <askzenai@gmail.com>
2025-02-10 02:54:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs only once after the component mounts
+    console.log('Component mounted!');
+    // If you have a cleanup function that should be executed when the component unmounts,
+    // you can return it from the useEffect function
+    return () => {
+      console.log('Component unmounted!');
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
